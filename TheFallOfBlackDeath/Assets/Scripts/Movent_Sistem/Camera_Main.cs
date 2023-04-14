e5b220e065f37dfbc57fa14ddda0cd29165a26bc
TheWizardSenpai <nanniniaugusto@gmail.com>
2023-04-14 13:38:12 -0300

    camara

---
+++ TheFallOfBlackDeath/Assets/Scripts/Movent_Sistem/Camera_Main.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Main : MonoBehaviour
+{
+    private Vector2 angle = new Vector2(-90 * Mathf.Deg2Rad,0);
+    
+    [SerializeField] private Transform Follow;
+    [SerializeField] private float Distance;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    void Update()
+    {
+        float Horizontal = Input.GetAxis("Mouse X");
+
+        if (Horizontal != 0)
+        {
+            angle.x += Horizontal * Mathf.Deg2Rad;
+        }
+
+        float Vertical = Input.GetAxis("Mouse Y");
+
+        if (Vertical != 0)
+        {
+            angle.y += Vertical * Mathf.Deg2Rad;
+        }
+    }
+
+    void LateUpdate()
+    {
+        Vector3 orbit = new Vector3(Mathf.Cos(angle.x) * Mathf.Cos(angle.y), -Mathf.Sin(angle.y), -Mathf.Sin(angle.x) * Mathf.Cos(angle.y));
+        
+        transform.position = Follow.position + orbit * Distance;
+        transform.rotation = Quaternion.LookRotation(Follow.position - transform.position);
+    }
+}
