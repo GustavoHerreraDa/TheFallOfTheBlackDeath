83af702a56464c064c54bc8e56f4930b9100a328
GustavoHerreraDa <gustavo.herrera@davinci.edu.ar>
2023-04-20 20:51:57 -0300

    Update merge

--- TheFallOfBlackDeath/Assets/Scripts/CombatSystem/CombatManager.cs
+++ TheFallOfBlackDeath/Assets/Scripts/CombatSystem/CombatManager.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public enum CombatStatus
 {
     WAITING_FOR_FIGHTER,
     FIGHTER_ACTION,
     CHECK_FOR_VICTORY,
-    NEXT_TURN
+    NEXT_TURN,
+    SKIP_TURN
 }
 
 public class CombatManager : MonoBehaviour
@@ -20,6 +21,11 @@ public class CombatManager : MonoBehaviour
 
     private Skill currentFighterSkill;
 
+    public int FighterIndex { get => fighterIndex; }
+
+    public List<PlayerFighter> playerFighters = new List<PlayerFighter>();
+    public List<EnemyFighter> enemyFighters = new List<EnemyFighter>();
+
     void Start()
     {
         LogPanel.Write("Battle initiated.");
@@ -27,6 +33,11 @@ public class CombatManager : MonoBehaviour
         foreach (var fgtr in this.fighters)
         {
             fgtr.combatManager = this;
+
+            if (fgtr.GetType() == typeof(PlayerFighter))
+                playerFighters.Add(fgtr.GetComponent<PlayerFighter>());
+            if (fgtr.GetType() == typeof(EnemyFighter))
+                enemyFighters.Add(fgtr.GetComponent<EnemyFighter>());
         }
 
         this.combatStatus = CombatStatus.NEXT_TURN;
@@ -62,18 +73,23 @@ public class CombatManager : MonoBehaviour
                     break;
 
                 case CombatStatus.CHECK_FOR_VICTORY:
+                    var countEnemyDown = 0;
                     foreach (var fgtr in this.fighters)
                     {
                         if (fgtr.isAlive == false)
                         {
-                            this.isCombatActive = false;
+                            countEnemyDown += 1;
+                        }
+                    }
+                    if (countEnemyDown == enemyFighters.Count)
+                    {
+                        this.isCombatActive = false;
 
-                            LogPanel.Write("Victory!");
-                        }
-                        else
-                        {
-                            this.combatStatus = CombatStatus.NEXT_TURN;
-                        }
+                        LogPanel.Write("Victory!");
+                    }
+                    else
+                    {
+                        this.combatStatus = CombatStatus.NEXT_TURN;
                     }
                     yield return null;
                     break;
@@ -89,25 +105,53 @@ public class CombatManager : MonoBehaviour
                     this.combatStatus = CombatStatus.WAITING_FOR_FIGHTER;
 
                     break;
+                case CombatStatus.SKIP_TURN:
+                    //yield return new WaitForSeconds(1f);
+                    this.fighterIndex = (this.fighterIndex + 1) % this.fighters.Length;
+                    currentTurn = this.fighters[this.fighterIndex];
+                    //LogPanel.Write($"{currentTurn.idName} has the turn.");
+                    currentTurn.InitTurn();
+                    this.combatStatus = CombatStatus.WAITING_FOR_FIGHTER;
+                    break;
             }
         }
     }
 
-    public Fighter GetOpposingFighter()
+    public Fighter GetOpposingCharacter()
     {
-        if (this.fighterIndex == 0)
+        foreach (var playerFighter in this.playerFighters)
         {
-            return this.fighters[2];
+            {
+                if (playerFighter.GetCurrentStats().health > 0)
+                {
+                    return this.fighters[2];
+                }
+            }
         }
-        else
+        return playerFighters[0];
+    }
+
+    public Fighter GetOpposingEnemy()
+    {
+        foreach (var enemyFighter in this.enemyFighters)
         {
-            return this.fighters[0];
+            if (enemyFighter.GetCurrentStats().health > 0)
+            {
+                return enemyFighter;
+            }
         }
+        return enemyFighters[0];
     }
 
     public void OnFighterSkill(Skill skill)
     {
         this.currentFighterSkill = skill;
         this.combatStatus = CombatStatus.FIGHTER_ACTION;
+    }
+
+    public void OnFighterDead()
+    {
+        //Debug.Log("Muerto");
+        this.combatStatus = CombatStatus.SKIP_TURN;
     }
 }
