a9207f8e69d6332895ff0e50d7c9b4fde2cbc617
GustavoHerreraDa <gustavo.herrera@davinci.edu.ar>
2023-04-21 17:44:33 -0300

    Update CCC Scene

--- TheFallOfBlackDeath/Assets/SkillManager.cs
+++ TheFallOfBlackDeath/Assets/SkillManager.cs
@@ -9,22 +9,49 @@ public class SkillManager : MonoBehaviour
     public int currentCharacterIndex;
     public GameObject currentCharacterObj;
 
+    public int SetSkill;
+
+    [Header("UI")]
+    public PlayerSkillPanel skillPanel;
+    public EnemySelectionPanel enemySelection;
+
+
     private void Awake()
     {
         combatManager = FindObjectOfType<CombatManager>();
+        enemySelection = FindObjectOfType<EnemySelectionPanel>();
+        skillPanel = FindObjectOfType<PlayerSkillPanel>();
     }
     void Start()
     {
-        currentCharacterIndex = combatManager.FighterIndex;
-        currentCharacterObj = combatManager.fighters[currentCharacterIndex].gameObject;
+        //currentCharacterIndex = combatManager.FighterIndex;
+        //currentCharacterObj = combatManager.fighters[currentCharacterIndex].gameObject;
     }
 
-    public void ExecuteSkill(int index)
+    public void SetExecuteSkill(int index)
     {
+        Debug.Log("SetExecuteSkill " + index);
+
         currentCharacterIndex = combatManager.FighterIndex;
 
         currentCharacterObj = combatManager.fighters[currentCharacterIndex].gameObject;
+        
+        skillPanel.Hide();
+        enemySelection.Show();
 
-        currentCharacterObj.GetComponent<PlayerFighter>().ExecuteSkill(index);
+
+        enemySelection.EnableButtonsWithEnemy();
+
+        SetSkill = index;
+    }
+
+    public void ExecuteSkill(int EnemyIndex)
+    {
+        combatManager.SetOpposingEnemy(EnemyIndex);
+
+        currentCharacterObj.GetComponent<PlayerFighter>().ExecuteSkill(SetSkill);
+
+        enemySelection.Hide();
+
     }
 }
