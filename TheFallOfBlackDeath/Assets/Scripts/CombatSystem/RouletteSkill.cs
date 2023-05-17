058536db8c72c85ff0abe379fbd59c4b9a4944cb
GustavoHerreraDa <gustavo.herrera@davinci.edu.ar>
2023-05-17 10:48:04 -0300

    Roulette Skill

---
+++ TheFallOfBlackDeath/Assets/Scripts/CombatSystem/RouletteSkill.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteSkill : MonoBehaviour
+{
+    Dictionary<Skill, int> _dic = new Dictionary<Skill, int>();
+
+    delegate void _actionDelegate();
+    public Skill attack;
+    public Skill speedUp;
+    public Skill shield;
+    public Skill electricity;
+
+
+    private void Start()
+    {
+        _dic.Add(attack, 50);
+        _dic.Add(speedUp, 20);
+        _dic.Add(shield, 20);
+        _dic.Add(electricity, 10);
+    }
+
+    public Skill Execute()
+    {
+        int totalWeight = 0;
+        foreach (var item in _dic)
+        {
+            totalWeight += item.Value;
+        }
+        int random = Random.Range(0, totalWeight);
+        foreach (var item in _dic)
+        {
+            random -= item.Value;
+            if (random < 0)
+            {
+                return item.Key;
+            }
+        }
+        return speedUp;
+    }
+}
