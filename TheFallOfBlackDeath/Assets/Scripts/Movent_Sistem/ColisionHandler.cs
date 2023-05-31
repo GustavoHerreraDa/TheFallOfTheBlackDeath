9f772cc9feca147f86cfb790c1aedf370fd605c1
FacundoFerreiro <129917786+FacundoFerreiro@users.noreply.github.com>
2023-05-31 20:30:27 -0300

    El zombie no atraviesa mas el piso

---
+++ TheFallOfBlackDeath/Assets/Scripts/Movent_Sistem/ColisionHandler.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionHandler : MonoBehaviour
+{
+    public string sceneToLoad;//Scene to load
+    public GameObject spawnPoint;
+    // GameState
+}
