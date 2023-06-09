f92710c0cf5cd9b8120ef1e7021f4c87524a76c9
FacundoFerreiro <129917786+FacundoFerreiro@users.noreply.github.com>
2023-06-09 16:00:24 -0300

    Limites para los buffos y los debuffs

--- TheFallOfBlackDeath/Assets/Scripts/CombatSystem/StatusMod.cs
+++ TheFallOfBlackDeath/Assets/Scripts/CombatSystem/StatusMod.cs
@@ -19,9 +19,31 @@ public class StatusMod : MonoBehaviour
         {
             case StatusModType.ATTACK_MOD:
                 modedStats.attack += this.amount;
+                if (modedStats.attack <= 25)
+                {
+                    modedStats.attack = 25;
+                    LogPanel.Write("You can't lower the attack any further. " + modedStats.attack);
+                }
+                if (modedStats.attack >= 80)
+                {
+                    modedStats.attack = 80;
+                    LogPanel.Write("You can't raise the attack any higher. " + modedStats.attack);
+                }
+  
                 break;
+
             case StatusModType.DEFFENSE_MOD:
                 modedStats.deffense += this.amount;
+                if (modedStats.deffense <= 20)
+                {
+                    modedStats.deffense = 20;
+                    LogPanel.Write("You can't lower the defense any further. " + modedStats.deffense);
+                }
+                if (modedStats.deffense >= 80)
+                {
+                    modedStats.deffense = 80;
+                    LogPanel.Write("The defense cannot be lowered further" + modedStats.deffense);
+                }
                 break;
         }
 
