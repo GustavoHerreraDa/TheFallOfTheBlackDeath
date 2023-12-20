cab276683b1a73cd25e13518802c451db33d1194
GustavoHerreraDa <gustavo.herrera@davinci.edu.ar>
2023-12-20 18:42:25 -0300

    Update Materiras and dressing for fortress interior

--- TheFallOfBlackDeath/Assets/TeleportPlayer.cs
+++ TheFallOfBlackDeath/Assets/TeleportPlayer.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class TeleportPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform destino; // El destino al que quieres mover al jugador
+
+    void OnTriggerStay(Collider other)
     {
-        
+        // Verificar si el objeto que está en el área del trigger es el jugador
+        //if (other.CompareTag("Charecter"))
+        //{
+            // Verificar si la tecla "E" está siendo presionada
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                MoverJugadorADestino(other.gameObject);
+            }
+        //}
     }
 
-    // Update is called once per frame
-    void Update()
+    void MoverJugadorADestino(GameObject player)
     {
-        
+        // Mover el jugador al destino
+        if (destino != null)
+        {
+            // Obtener el componente de transformación del jugador
+            //Transform jugadorTransform = transform;
+
+            // Establecer la posición del jugador en la posición del destino
+            player.transform.position = destino.position;
+
+            Debug.Log("Jugador movido a la posición del destino.");
+        }
+        else
+        {
+            Debug.LogError("El destino no está asignado. Asigna un objeto de destino en el Inspector.");
+        }
     }
 }
