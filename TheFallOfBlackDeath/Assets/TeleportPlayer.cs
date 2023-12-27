c4ab5c49dc2f611e188f21497fb117997df252a8
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2023-12-27 01:06:26 -0300

    Portal fix

--- TheFallOfBlackDeath/Assets/TeleportPlayer.cs
+++ TheFallOfBlackDeath/Assets/TeleportPlayer.cs
@@ -4,37 +4,21 @@ using UnityEngine;
 
 public class TeleportPlayer : MonoBehaviour
 {
-    public Transform destino; // El destino al que quieres mover al jugador
+    public GameObject player;
+    public Transform destino;
+    private CharacterController _characterController;
 
-    void OnTriggerStay(Collider other)
+    void Start()
     {
-        // Verificar si el objeto que está en el área del trigger es el jugador
-        //if (other.CompareTag("Charecter"))
-        //{
-            // Verificar si la tecla "E" está siendo presionada
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                MoverJugadorADestino(other.gameObject);
-            }
-        //}
+        _characterController = player.GetComponent<CharacterController>();
     }
 
-    void MoverJugadorADestino(GameObject player)
+    void OnTriggerEnter(Collider other)
     {
-        // Mover el jugador al destino
-        if (destino != null)
-        {
-            // Obtener el componente de transformación del jugador
-            //Transform jugadorTransform = transform;
-
-            // Establecer la posición del jugador en la posición del destino
-            player.transform.position = destino.position;
-
-            Debug.Log("Jugador movido a la posición del destino.");
-        }
-        else
-        {
-            Debug.LogError("El destino no está asignado. Asigna un objeto de destino en el Inspector.");
-        }
+        _characterController.enabled = false; //Es necesario desactivar el character controller antes de teletransportar al PJ.
+        player.transform.position = destino.position;
+        _characterController.enabled = true;
     }
+
+    
 }
